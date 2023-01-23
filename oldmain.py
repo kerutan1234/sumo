518e761481cf08a9d7f8c536bdfe5705c7857ed3
kerutan1234 <85813224+kerutan1234@users.noreply.github.com>
2023-01-23 11:31:54 +0900

    test

--- oldmain.py
+++ oldmain.py
@@ -8,6 +8,10 @@ import pandas as pd
 url = "https://suumo.jp/jj/common/ichiran/JJ901FC004/?initFlg=1&seniFlg=1&ar=030&ta=14&scTmp=14132&ct=9999999&cb=0.0&kt=9999999&xt=9999999&et=9999999&cn=9999999&newflg=0&km=1&sc=14132&bs=040&pc=100"
 res = requests.get(url)
 soup = BeautifulSoup(res.content, "html.parser")
+
+# print(soup.select("h1"))
+
+# print(soup.select("p"))
 house_list = []
 elements = soup.find_all("li", class_='cassette js-bukkenCassette')
 for i in elements:
@@ -16,10 +20,9 @@ for i in elements:
     dictemp = {}
     detail_titles = i.find_all("div", class_='infodatabox-box-title')
     detail_contents = i.find_all("div", class_='infodatabox-box-txt')
-    #bcid=物件タグの取得
-    bc_id_tag = i.find('input', class_="js-ikkatsuCB")
-    bcid = bc_id_tag['value']
-    #print(bcid)
+    bc_id = i.find("div", class_='ui-media-object infodatabox-object')
+
+    print(bc_id)
     for detail_title in detail_titles:
         temp1.append(detail_title.get_text())
 
@@ -29,12 +32,12 @@ for i in elements:
         temp2.append(ii)
     for title, result in zip(temp1, temp2):
         dictemp[title] = result
-    house_list.append({bcid:dictemp})
+    house_list.append(dictemp)
 
 #jsonに保存している
 with open("data.json", "w") as f:
     json.dump(house_list, f, sort_keys=True, indent=4)
-pprint.pprint(house_list)
+print(house_list)
 #保存したJSONをデータフレームにしている
 df=pd.read_json("data.json",encoding='utf-8')
 #print(df)
