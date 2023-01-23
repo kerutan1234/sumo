844bdb49110eaec90ff5240ca60764fe201e6a39
kerutan1234 <85813224+kerutan1234@users.noreply.github.com>
2023-01-23 14:02:48 +0900

    最初に作った一枚だけ画像が生成できる

---
+++ make_png.py
@@ -0,0 +1,71 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+from PIL import Image,ImageDraw, ImageFont
+import json
+import textwrap
+
+
+play_path = "C:/Users/newscorpion/Documents/work/sumo/drawing/100220872649.jpg"
+im1 = Image.open(play_path).copy()
+json_open = open('./data.json', 'r')
+json_load=json.load(json_open)
+#print(json_load)
+test_one=json_load[0]
+print(test_one)
+#icon_path = 'C:/Users/newscorpion/Documents/work/sumo/drawing/100301143101.jpg'
+#im2 = Image.open(icon_path).copy()
+
+
+def get_concat_h_blank(im1, color=(255, 255, 255)):
+
+
+
+    dst = Image.new('RGB', (im1.width + 300, max(im1.height, 300)), color)
+    dst.paste(im1, (300, 0))
+    draw = ImageDraw.Draw(dst)
+    font=ImageFont.truetype('C:/Windows/Fonts/msgothic.ttc', 30)
+    font2=ImageFont.truetype('C:/Windows/Fonts/msgothic.ttc', 25)
+    line_counter = 0
+    wrap_list_address = textwrap.wrap(test_one["住所"], 8)
+    for line in wrap_list_address:
+        y=line_counter*35+20
+        draw.multiline_text((10, y),line, fill=(0,0,0), font=font)
+        line_counter = line_counter +1  # 行数のカウンターに1
+    line_counter = 0
+    split_fee=test_one["価格（月額のローン支払い）"].split("円")
+    del split_fee[-1]
+    for line in split_fee:
+        y=line_counter*35+225
+        draw.multiline_text((10, y),line+"円", fill=(0,0,0), font=font2)
+        line_counter = line_counter +1  # 行数のカウンターに1
+    line_counter = 0
+    wrap_list_time = textwrap.wrap(test_one["築年数・入居時期"], 8)
+    for line in wrap_list_time:
+        y=line_counter*35+350
+        draw.multiline_text((10, y),"築年数:"+line, fill=(0,0,0), font=font2)
+        line_counter = line_counter +1  # 行数のカウンターに1
+    line_counter = 0
+    split_madori=test_one["間取り/面積/総戸数"].split("間取り")
+    for line in split_madori:
+        y=line_counter*35+400
+        if line_counter==1:
+            draw.multiline_text((10, y),"間取り"+line, fill=(0,0,0), font=font2)
+        else:
+            draw.multiline_text((10, y),line, fill=(0,0,0), font=font2)
+        line_counter = line_counter +1  # 行数のカウンターに1
+
+    return dst
+
+def get_concat_v_blank(im1, im2, color=(0, 0, 0)):
+    dst = Image.new('RGB', (max(im1.width, im2.width), im1.height + im2.height), color)
+    dst.paste(im1, (0, 0))
+    dst.paste(im2, (0, im1.height))
+    return dst
+
+get_concat_h_blank(im1).save('data/dst/pillow_concat_h_blank.jpg')
+split_fee=test_one["価格（月額のローン支払い）"].split("円")
+print(split_fee)
+for i in split_fee:
+    print(i+"円")
+
+#get_concat_v_blank(im1, im2, (0, 64, 128)).save('data/dst/pillow_concat_v_blank.jpg')
