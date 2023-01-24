344240a0d33eafdd3ba28961368e4634c80a13ee
kerutan1234 <85813224+kerutan1234@users.noreply.github.com>
2023-01-24 14:03:40 +0900

    resize関連

--- make_png.py
+++ make_png.py
@@ -3,6 +3,8 @@
 from PIL import Image,ImageDraw, ImageFont
 import json
 import textwrap
+import resize
+from tqdm import tqdm
 
 
 
@@ -12,41 +14,59 @@ bc_lists=json_load
 
 
 def get_concat_h_blank(im1,bc_list, color=(255, 255, 255)):
-    dst = Image.new('RGB', (im1.width + 300, max(im1.height, 300)), color)
-    dst.paste(im1, (300, 0))
+    dst = Image.new('RGB', (1980, 1080), color)
+
+    im1=resize.keepAspectResizeSimple(im1,bc_list["bcid"])#リサイズ
+
+    img_w=im1.width
+    img_h=im1.height
+    if img_w>img_h:
+        dst.paste(im1, (990, int(float(1080-img_h)/2)))
+    elif img_w<img_h:
+        dst.paste(im1, (990+int(float(990-img_w)/2), 0))
+    elif img_w==img_h:
+        dst.paste(im1, (990+int(float(990-img_w)/2), int(float(1080-img_h)/2)))
+    else:
+        print("リサイズエラー")
     draw = ImageDraw.Draw(dst)
-    font=ImageFont.truetype('C:/Windows/Fonts/msgothic.ttc', 30)
-    font2=ImageFont.truetype('C:/Windows/Fonts/msgothic.ttc', 25)
+######################################描写文字設定
+    font=ImageFont.truetype('C:/Windows/Fonts/msgothic.ttc', 120)
+    font2=ImageFont.truetype('C:/Windows/Fonts/msgothic.ttc', 70)
+    font3=ImageFont.truetype('C:/Windows/Fonts/msgothic.ttc', 100)
+
     line_counter = 0
-    wrap_list_address = textwrap.wrap(bc_list["住所"], 8)
-    print(bc_list)
+    wrap_list_address=bc_list["住所"].split("市",1)
     for line in wrap_list_address:
-        y=line_counter*35+20
-        draw.multiline_text((10, y),line, fill=(0,0,0), font=font)
+        y=line_counter*120+20
+        if line_counter==0:
+            draw.multiline_text((10, y),line+"市", fill=(0,0,0), font=font)
+        else:
+            draw.multiline_text((10, y),line, fill=(0,0,0), font=font)
         line_counter = line_counter +1  # 行数のカウンターに1
     line_counter = 0
-    split_fee=bc_list["価格（月額のローン支払い）"].split("円")
+    split_fee=bc_list["価格（月額のローン支払い）"].split("円",1)#ここの,1をとると全部表示されるよ
     del split_fee[-1]
+    print(split_fee)
     for line in split_fee:
-        y=line_counter*35+225
-        draw.multiline_text((10, y),line+"円", fill=(0,0,0), font=font2)
+        y=line_counter*100+500###################################高さの位置指定
+        draw.multiline_text((10, y),line+"円", fill=(0,0,0), font=font3)
         line_counter = line_counter +1  # 行数のカウンターに1
     line_counter = 0
     wrap_list_time = textwrap.wrap(bc_list["築年数・入居時期"], 8)
     for line in wrap_list_time:
-        y=line_counter*35+350
+        y=line_counter*70+350####################################高さの位置指定
         draw.multiline_text((10, y),"築年数:"+line, fill=(0,0,0), font=font2)
         line_counter = line_counter +1  # 行数のカウンターに1
     line_counter = 0
     split_madori=bc_list["間取り/面積/総戸数"].split("間取り")
     for line in split_madori:
-        y=line_counter*35+400
+        y=line_counter*70+900#####################################高さの位置指定
         if line_counter==1:
             draw.multiline_text((10, y),"間取り"+line, fill=(0,0,0), font=font2)
         else:
             draw.multiline_text((10, y),line, fill=(0,0,0), font=font2)
         line_counter = line_counter +1  # 行数のカウンターに1
-
+    dst.save("./debug/"+bc_list["bcid"]+"_dst.jpg")
     return dst
 
 def get_concat_v_blank(im1, im2, color=(0, 0, 0)):
@@ -54,8 +74,7 @@ def get_concat_v_blank(im1, im2, color=(0, 0, 0)):
     dst.paste(im1, (0, 0))
     dst.paste(im2, (0, im1.height))
     return dst
-for bc_list in bc_lists:
-    #print(bc_list)
+for bc_list in tqdm(bc_lists, leave=False):
     play_path = "./drawing/"+bc_list["bcid"]+".jpg"
     im1 = Image.open(play_path).copy()
     get_concat_h_blank(im1,bc_list).save('data/'+bc_list["bcid"]+'.jpg')
---
+++ resize.py
@@ -0,0 +1,20 @@
+import os
+from PIL import Image
+
+def keepAspectResizeSimple(image,id):#imageオブジェクトを渡すこと
+    name=id+".jpg"
+    size=(980,1080)
+    # 画像の読み込み
+    # サイズを幅と高さにアンパック
+    width, height = size
+    # 矩形と画像の幅・高さの比率の小さい方を拡大率とする
+    ratio = min(width / image.width, height / image.height)
+    # 画像の幅と高さに拡大率を掛けてリサイズ後の画像サイズを算出
+    resize_size = (round(ratio * image.width), round(ratio * image.height))
+    # リサイズ後の画像サイズにリサイズ
+    resized_image = image.resize(resize_size)
+    resized_image.save("./debug/"+id+"_resize.jpg")
+    resized_image.save("./drawing/resize/"+name)
+    return resized_image
+if __name__ == '__main__':
+    pass
